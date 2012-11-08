d75cc033b09db7e76a23bfe81385be098859abf6
Claus Kløve <clauskloeve@gmail.com>
2012-11-08 13:29:42 +0100

    sdfgdfg

--- js/appsfrog.js
+++ js/appsfrog.js
@@ -1,4 +1,29 @@
-function right1() {
-	document.getElementById("sound").play();
-	$('#go').animate({"opacity": "1"}, "fast");
-};
+$('#page2').live('pageinit',function(){
+
+    $("#playaudio").live('tap', function() {
+        // Note: two ways to access media file: web and local file        
+        var src = '../res/sounds/Frog_Croak.wav';
+   
+  
+        
+        // local (on device): copy file to project's /assets folder:
+        // var src = '/android_asset/spittinggames.m4a';
+        
+        playAudio(src);
+		
+		$('#go').animate({"opacity": "1"}, "fast");
+		
+    });
+
+    $("#pauseaudio").live('tap', function() {
+        pauseAudio();
+    });
+    
+    $("#stopaudio").live('tap', function() {
+        stopAudio();
+    });
+});
+
+
+
+
