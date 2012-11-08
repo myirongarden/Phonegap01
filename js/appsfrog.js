236e4a4afe64140fa21c84a629c8c03ab08136e2
Claus Kløve <clauskloeve@gmail.com>
2012-11-08 13:05:28 +0100

    fghfhf

--- js/appsfrog.js
+++ js/appsfrog.js
@@ -1,12 +1,3 @@
-function pageLoad() {
-	
-  document.addEventListener("deviceready", function(){
-    
-		
-  });
-  
-}
-
 function right1() {
 	document.getElementById("sound").play();
 	$('#go').animate({"opacity": "1"}, "fast");
