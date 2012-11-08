c1af472a8bf69eca5874862f2519073d271b11c8
Claus Kløve <clauskloeve@gmail.com>
2012-11-08 15:09:15 +0100

    dfdg

--- js/appsfrog.js
+++ js/appsfrog.js
@@ -1,15 +1,14 @@
-$('#page2').live('pageinit',function(){
 
-    $("#playaudio").live('tap', function() {
+
+    $("#imgfrog").live('tap', function() {
         // Note: two ways to access media file: web and local file        
-        var src = '../res/sounds/Frog_Croak.wav';
-   
+
   
         
         // local (on device): copy file to project's /assets folder:
         // var src = '/android_asset/spittinggames.m4a';
         
-        playAudio(src);
+        playAudio('../res/sounds/Frog_Croak.wav');
 		
 		$('#go').animate({"opacity": "1"}, "fast");
 		
@@ -22,8 +21,80 @@ $('#page2').live('pageinit',function(){
     $("#stopaudio").live('tap', function() {
         stopAudio();
     });
-});
 
 
 
+
+
+// Audio player
+        //
+        var my_media = null;
+        var mediaTimer = null;
+
+        // Play audio
+        //
+        function playAudio(src) {
+            // Create Media object from src
+            my_media = new Media(src, onSuccess, onError);
+
+            // Play audio
+            my_media.play();
+
+            // Update my_media position every second
+            if (mediaTimer == null) {
+                mediaTimer = setInterval(function() {
+                    // get my_media position
+                    my_media.getCurrentPosition(
+                        // success callback
+                        function(position) {
+                            if (position > -1) {
+                                setAudioPosition((position) + " sec");
+                            }
+                        },
+                        // error callback
+                        function(e) {
+                            console.log("Error getting pos=" + e);
+                            setAudioPosition("Error: " + e);
+                        }
+                    );
+                }, 1000);
+            }
+        }
+
+        // Pause audio
+        // 
+        function pauseAudio() {
+            if (my_media) {
+                my_media.pause();
+            }
+        }
+
+        // Stop audio
+        // 
+        function stopAudio() {
+            if (my_media) {
+                my_media.stop();
+            }
+            clearInterval(mediaTimer);
+            mediaTimer = null;
+        }
+
+        // onSuccess Callback
+        //
+        function onSuccess() {
+            console.log("playAudio():Audio Success");
+        }
+
+        // onError Callback 
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' + 
+                  'message: ' + error.message + '\n');
+        }
+
+        // Set audio position
+        // 
+        function setAudioPosition(position) {
+            document.getElementById('audio_position').innerHTML = position;
+        }
 
