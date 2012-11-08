8bd0e4fc97184405dd07365cfda510d6360441c2
Claus Kløve <clauskloeve@gmail.com>
2012-11-08 12:29:00 +0100

    sfsdf

---
+++ js/appsfrog.js
@@ -0,0 +1,13 @@
+function pageLoad() {
+	
+  document.addEventListener("deviceready", function(){
+    
+		
+  });
+  
+}
+
+function right1() {
+	document.getElementById("sound").play();
+	$('#go').animate({"opacity": "1"}, "fast");
+};
