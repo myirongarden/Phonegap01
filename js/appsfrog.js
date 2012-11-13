70a405c99db63dcf568a1d38d0a75c64f6403a57
Claus Kløve <clauskloeve@gmail.com>
2012-11-13 12:39:03 +0100

    sdfssdf

--- js/appsfrog.js
+++ js/appsfrog.js
@@ -1,26 +1,19 @@
-function frogevent() {
-        playAudio('/android_asset/Frog_Croak.wav');
-		
-		$('#go').animate({"opacity": "1"}, "fast");
-
-}
-
 function init() {
 
 
-      
+	document.getElementById("imgfrog").addEventListener("onclick", frogevent, false);
 
-document.getElementById("imgfrog").addEventListener("click", frogevent, false);
+		alert("init");
+
+}
 
 
-    $("#pauseaudio").live('tap', function() {
-        pauseAudio();
-    });
-    
-    $("#stopaudio").live('tap', function() {
-        stopAudio();
-    });
-
+function frogevent() {
+        playAudio('Frog_Croak.wav');
+		
+		$('#go').animate({"opacity": "1"}, "fast");
+		
+		alert("dav");
 
 }
 
@@ -30,70 +23,22 @@ document.getElementById("imgfrog").addEventListener("click", frogevent, false);
         var my_media = null;
         var mediaTimer = null;
 
-        // Play audio
-        //
         function playAudio(src) {
-            // Create Media object from src
-            my_media = new Media(src, onSuccess, onError);
-
-            // Play audio
-            my_media.play();
-
-            // Update my_media position every second
-            if (mediaTimer == null) {
-                mediaTimer = setInterval(function() {
-                    // get my_media position
-                    my_media.getCurrentPosition(
-                        // success callback
-                        function(position) {
-                            if (position > -1) {
-                                setAudioPosition((position) + " sec");
-                            }
-                        },
-                        // error callback
-                        function(e) {
-                            console.log("Error getting pos=" + e);
-                            setAudioPosition("Error: " + e);
-                        }
-                    );
-                }, 1000);
+            if (device.platform == 'Android') {
+                src = '/android_asset/www/' + src;
             }
+
+            var media = new Media(src, success, error_error);
+
+            media.play();
         }
 
-        // Pause audio
-        // 
-        function pauseAudio() {
-            if (my_media) {
-                my_media.pause();
-            }
+        function success() {
+            // ignore
         }
 
-        // Stop audio
-        // 
-        function stopAudio() {
-            if (my_media) {
-                my_media.stop();
-            }
-            clearInterval(mediaTimer);
-            mediaTimer = null;
-        }
-
-        // onSuccess Callback
-        //
-        function onSuccess() {
-            console.log("playAudio():Audio Success");
-        }
-
-        // onError Callback 
-        //
-        function onError(error) {
-            alert('code: '    + error.code    + '\n' + 
-                  'message: ' + error.message + '\n');
-        }
-
-        // Set audio position
-        // 
-        function setAudioPosition(position) {
-            document.getElementById('audio_position').innerHTML = position;
+        function error_error(e) {
+            alert('great error');
+            alert(e.message);
         }
 
