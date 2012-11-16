0fe123906fd5e4a6518ff4d496e882aa7b254cf0
Claus Kløve <clauskloeve@gmail.com>
2012-11-16 21:08:55 +0100

    fdfgd

--- appsfrog.js
+++ appsfrog.js
@@ -6,14 +6,15 @@ var watchID = null;
 
 function startWatch() {
 
-	// Update compass every 3 seconds
-	var options = { frequency: 3000 };
+	var options = { frequency: 1000 };
 
 	watchID = navigator.compass.watchHeading(onSuccess, onError, options);
 }
 function onSuccess(heading) {
-	var element = document.getElementById('heading');
-	element.innerHTML = 'Heading: ' + heading;
+	var compassElement = document.getElementById('compass');
+	
+	compassElement.setAttribute('style', 'rotation:'+heading+'deg;');
+	
 }
 
 // onError: Failed to get the heading
@@ -24,30 +25,30 @@ function onError() {
 
 
 function frogevent() {
-        playAudio("sounds/Frog_Croak.wav");
+	playAudio("sounds/Frog_Croak.wav");
 
 }
 
 
 // Audio player
-        //
-        var my_media = null;
+//
+var my_media = null;
 
-        function playAudio(src) {
-            if (device.platform == 'Android') {
-                src = '/android_asset/www/' + src;
-            }
+function playAudio(src) {
+	if (device.platform == 'Android') {
+		src = '/android_asset/www/' + src;
+	}
 
-            var media = new Media(src, success, error_error);
+	var media = new Media(src, success, error_error);
 
-            media.play();
-        }
+	media.play();
+}
 
-        function success() {
-            // ignore
-        }
+function success() {
+	// ignore
+}
 
-        function error_error(e) {
-            alert(e.message);
-        }
+function error_error(e) {
+	alert(e.message);
+}
 
